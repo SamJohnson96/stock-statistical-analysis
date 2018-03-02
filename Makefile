82ede864aabbf046854848561b435bdfadf8a000
Sam Johnson <sam@3ev.com>
2018-03-02 21:00:44 +0000

    new apple update file

--- Makefile
+++ Makefile
@@ -3,15 +3,15 @@ AWS_REGION = eu-west-2
 LAMBDA_ROLE = arn:aws:iam::329627156298:role/service-role/lambda_basic
 
 # COLLECT RESULTS FUNCTION
-COLLECT_RESULTS_FUNCTION_NAME = arn:aws:lambda:eu-west-2:329627156298:function:CollectResults
-COLLECT_RESULTS_FILE_NAME = collect_results
-COLLECT_RESULTS_FUNCTION_HANDLER = lambda_handler
+UPDATE-APPLE-PREDICTION_FUNCTION_NAME = arn:aws:lambda:eu-west-2:329627156298:function:UpdateApplePrediction
+UPDATE-APPLE-PREDICTION_FILE_NAME = update-apple-prediction
+UPDATE-APPLE-PREDICTION_FUNCTION_HANDLER = lambda_handler
 
 # BUILD AND CREATE PACKAGES
-build_collect_results: clean_package organise_collect_results
+build_update-apple-prediction: clean_package organise_update-apple-prediction
 
 # BUILD - DELETE - CREATE
-refresh_collect_results: build_collect_results collect_results_delete collect_results_create
+refresh_update-apple-prediction: build_update-apple-prediction update-apple-prediction_delete update-apple-prediction_create
 
 # CLEAN BUILD
 clean_package:
@@ -19,39 +19,39 @@ clean_package:
 	sudo rm -rf build
 
 # DOWNLOAD ALL PACKAGES AND FILES NEEDED FOR DEPLOYMENT FOR METHODS
-organise_collect_results:
+organise_update-apple-prediction:
 	# Make site-packages
 	mkdir -p build/site-packages
 
 	# Move
-	cp stock-statistical-analysis/collect_results.py build/site-packages
+	cp stock-statistical-analysis/update-apple-prediction.py build/site-packages
 
 	# Create virtual environment in build/scrape
-	virtualenv -p /usr/bin/python3.4 build/collect_results
+	virtualenv -p /usr/bin/python3.4 build/update-apple-prediction
 
 	# Activate the virtual environment
-	. build/collect_results/bin/activate; \
+	. build/update-apple-prediction/bin/activate; \
 
 	# Install dependencies in virtual environment
 	# sudo python3 -m pip install -U requests -t build/site-packages/
 	# sudo python3 -m pip install -U boto3 -t build/site-packages/
 
   # Move to build/site-packages
-	cd build/site-packages; zip -g -r ../collect_results.zip . -x "*__pycache__*"
+	cd build/site-packages; zip -g -r ../update-apple-prediction.zip . -x "*__pycache__*"
 
 # CREATION AWS CLI CALLS FOR EVERY METHOD.
-collect_results_create:
+update-apple-prediction_create:
 	aws lambda create-function \
 		--region $(AWS_REGION) \
 		--role $(LAMBDA_ROLE) \
-		--function-name $(COLLECT_RESULTS_FUNCTION_NAME) \
-		--zip-file fileb://./build/collect_results.zip \
-		--handler $(COLLECT_RESULTS_FILE_NAME).$(COLLECT_RESULTS_FUNCTION_HANDLER) \
+		--function-name $(UPDATE-APPLE-PREDICTION_FUNCTION_NAME) \
+		--zip-file fileb://./build/update-apple-prediction.zip \
+		--handler $(UPDATE-APPLE-PREDICTION_FILE_NAME).$(UPDATE-APPLE-PREDICTION_FUNCTION_HANDLER) \
 		--runtime python3.6 \
 		--timeout 15 \
 		--memory-size 128
 
 # DELETION AWS CLI CALLS FOR EVERY METHOD
-collect_results_delete:
+update-apple-prediction_delete:
 	aws lambda delete-function \
-		--function-name $(COLLECT_RESULTS_FUNCTION_NAME)
+		--function-name $(UPDATE-APPLE-PREDICTION_FUNCTION_NAME)
---
+++ stock-statistical-analysis/update-apple-prediction.py
@@ -0,0 +1,246 @@
+import imp
+import json
+import sys
+import boto3
+import time
+import numpy as np
+from datetime import datetime
+
+def lambda_handler(event, context):
+    article_id = get_new_article_id(event)
+    article = wait_for_all_classifications(article_id, 0)
+    if article is None:
+        print ('Issue with collecting classifications')
+    else:
+        update_hour(article)
+        update_day(article)
+        update_week(article)
+        update_month(article)
+
+def get_new_article_id(event):
+    article = []
+    for record in event['Records']:
+        if 'NewImage' in record['dynamodb']:
+            article['article_id'] = record['dynamodb']['NewImage']['article_id']['N']
+        else:
+            print('Not a new event.')
+            return;
+
+def wait_for_all_classifications(article_id, retry):
+    # Configuration for database
+    dynamodb = boto3.resource('dynamodb')
+    table = 'apple_article_results'
+    pk_key = 'article_id'
+
+    # Get row
+    response = table.get_item(Key={pk_key: int(article_id)})
+    article = response['Item']
+
+    if 'naive_bayes' in response.keys() and 'naive_bayes' in response.keys():
+        return article
+    elif retry < 5:
+        #If they're not all there then wait.
+        time.sleep(3)
+        return wait_for_all_classifications(article_id,retry+1)
+    else:
+        return None
+
+
+def update_hour(article):
+    print('--- Updating hour ---')
+    # Configuration for database
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('apple_article_results')
+
+    article_id = article['article_id']
+    twenty_four_hours_ago = article_id - 3600
+
+    response = table.scan(
+                        Select='ALL_ATTRIBUTES',
+                        FilterExpression=Key('article_id').between(int(twenty_four_hours_ago),int(article_id))
+             )
+
+    # Get SVM classification
+    svm_classification = article['naive_bayes']
+    update_svm_classification('hour',response,svm_classification)
+
+    # Get Naive Bayes classification
+    naive_bayes_classification = article['naive_bayes']
+    update_naive_bayes_classification('hour',response,naive_bayes_classification)
+
+    # Work out average classification
+    average_prediction = get_average('hour'response,naive_bayes_classification, svm_classification)
+
+def update_day(article):
+    print('--- Updating day ---')
+    # Configuration for database
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('apple_article_results')
+
+    article_id = article['article_id']
+    twenty_four_hours_ago = article_id - (24*3600)
+
+    response = table.scan(
+                        Select='ALL_ATTRIBUTES',
+                        FilterExpression=Key('article_id').between(int(twenty_four_hours_ago),int(article_id))
+             )
+
+    # Get SVM classification
+    svm_classification = article['naive_bayes']
+    update_svm_classification('day',response,svm_classification)
+
+    # Get Naive Bayes classification
+    naive_bayes_classification = article['naive_bayes']
+    update_naive_bayes_classification('day',response,naive_bayes_classification)
+
+    # Work out average classification
+    average_prediction = get_average('day',response,naive_bayes_classification, svm_classification)
+
+def update_week(article):
+    print('--- Updating week ---')
+    # Configuration for database
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('apple_article_results')
+
+    article_id = article['article_id']
+    twenty_four_hours_ago = article_id - (7*(24*3600))
+
+    response = table.scan(
+                        Select='ALL_ATTRIBUTES',
+                        FilterExpression=Key('article_id').between(int(twenty_four_hours_ago),int(article_id))
+             )
+
+    # Get SVM classification
+    svm_classification = article['naive_bayes']
+    update_svm_classification('week',response,svm_classification)
+
+    # Get Naive Bayes classification
+    naive_bayes_classification = article['naive_bayes']
+    update_naive_bayes_classification('week',response,naive_bayes_classification)
+
+    # Work out average classification
+    average_prediction = get_average('week',response,naive_bayes_classification, svm_classification)
+
+def update_month(article):
+    print('--- Updating hour ---')
+    # Configuration for database
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('apple_article_results')
+
+    article_id = article['article_id']
+    twenty_four_hours_ago = article_id - (31*(24*3600))
+
+    response = table.scan(
+                        Select='ALL_ATTRIBUTES',
+                        FilterExpression=Key('article_id').between(int(twenty_four_hours_ago),int(article_id))
+             )
+
+    # Get SVM classification
+    svm_classification = article['naive_bayes']
+    update_svm_classification('month',response,svm_classification)
+
+    # Get Naive Bayes classification
+    naive_bayes_classification = article['naive_bayes']
+    update_naive_bayes_classification('month',response,naive_bayes_classification)
+
+    # Work out average classification
+    average_prediction = update_average('month',response,  naive_bayes_classification, svm_classification)
+
+def update_svm_classification(measure, results, new_classification):
+    results = results['Items']
+    #Go through each result and get svm classification row
+    svm_classification = []
+    svm_classification.append(new_classification)
+    for result in results:
+        svm_classification.append(result['naive_bayes'])
+    updated_prediction = get_highest_freq(svm_classification)
+
+    if measure == 'hour':
+        row = 'naive_bayes_hourly'
+    elif measure == 'day':
+        row = 'naive_bayes_daily'
+    elif measure == 'week':
+        row = 'naive_bayes_weekly'
+    elif measure == 'month':
+        row = 'naive_bayes_monthly'
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('apple_article_results')
+    table.update_item(
+        Key={
+            'sector': 'apple',
+        },
+        UpdateExpression='SET :val1 = :val2',
+        ExpressionAttributeValues={
+            ':val1': row
+            ':val2': int(updated_prediction)
+        }
+    )
+
+def update_naive_bayes_classification(measure, results, new_classification):
+    results = results['Items']
+    #Go through each result and get svm classification row
+    naive_bayes_classification = []
+    naive_bayes_classification.append(new_classification)
+    for result in results:
+        naive_bayes_classification.append(result['naive_bayes'])
+    updated_prediction = get_highest_freq(naive_bayes_classification)
+
+    if measure == 'hour':
+        row = 'naive_bayes_hourly'
+    elif measure == 'day':
+        row = 'naive_bayes_daily'
+    elif measure == 'week':
+        row = 'naive_bayes_weekly'
+    elif measure == 'month':
+        row = 'naive_bayes_monthly'
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('apple_article_results')
+    table.update_item(
+        Key={
+            'sector': 'apple',
+        },
+        UpdateExpression='SET :val1 = :val2',
+        ExpressionAttributeValues={
+            ':val1': row
+            ':val2': int(updated_prediction)
+        }
+    )
+
+def update_average(measure, results, naive_bayes, svm_classification):
+    results = results['Items']
+    #Go through each result and get svm classification row
+    all_classification = []
+    all_classification.append(naive_bayes)
+    all_classification.append(svm_classification)
+    for result in results:
+        all_classification.append(result['naive_bayes'])
+        all_classification.append(result['support_vector_machine'])
+
+    updated_prediction = get_highest_freq(all_classification)
+
+    if measure == 'hour':
+        row = 'total_average_hourly'
+    elif measure == 'day':
+        row = 'total_average_daily'
+    elif measure == 'week':
+        row = 'total_average_weekly'
+    elif measure == 'month':
+        row = 'total_average_monthly'
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('apple_article_results')
+    table.update_item(
+        Key={
+            'sector': 'apple',
+        },
+        UpdateExpression='SET :val1 = :val2',
+        ExpressionAttributeValues={
+            ':val1': row
+            ':val2': int(updated_prediction)
+        }
+    )
+
+def get_highest_freq(classifications):
+    return np.bincount(classifications).argmax()
