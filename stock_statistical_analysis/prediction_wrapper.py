b66f96389d22ef787fb596572035a16dca73be10
Sam Johnson <sam@3ev.com>
2018-03-26 18:48:07 +0100

    prediction wrapper

--- stock_statistical_analysis/prediction_wrapper.py
+++ stock_statistical_analysis/prediction_wrapper.py
@@ -15,6 +15,9 @@ class PredictionWrapper:
             predictions['TOTAL'] = response['Item']['total_average_hourly']
             predictions['NAIVE_BAYES'] = response['Item']['naive_bayes_hourly']
             predictions['SUPPORT_VECTOR_MACHINE'] = response['Item']['support_vector_machine_hourly']
+            predictions['K_NEIGHBORS_MACHINE'] = response['Item']['k_nearest_hourly']
+            predictions['LINEAR_PERCEPTRON'] = response['Item']['linear_perceptron_hourly']
+            predictions['EXTRA_TREES'] = response['Item']['extra_trees_hourly']
             predictions['HAS_CHANGED'] = response['Item']['hour_change']
             company_predictions[sector] = predictions
         return company_predictions
@@ -27,6 +30,9 @@ class PredictionWrapper:
             predictions['TOTAL'] = response['Item']['total_average_daily']
             predictions['NAIVE_BAYES'] = response['Item']['naive_bayes_daily']
             predictions['SUPPORT_VECTOR_MACHINE'] = response['Item']['support_vector_machine_daily']
+            predictions['K_NEIGHBORS_MACHINE'] = response['Item']['k_nearest_daily']
+            predictions['LINEAR_PERCEPTRON'] = response['Item']['linear_perceptron_daily']
+            predictions['EXTRA_TREES'] = response['Item']['extra_trees_daily']
             predictions['HAS_CHANGED'] = response['Item']['day_change']
             company_predictions[sector] = predictions
         return company_predictions
@@ -39,6 +45,9 @@ class PredictionWrapper:
             predictions['TOTAL'] = response['Item']['total_average_weekly']
             predictions['NAIVE_BAYES'] = response['Item']['naive_bayes_weekly']
             predictions['SUPPORT_VECTOR_MACHINE'] = response['Item']['support_vector_machine_weekly']
+            predictions['K_NEIGHBORS_MACHINE'] = response['Item']['k_nearest_weekly']
+            predictions['LINEAR_PERCEPTRON'] = response['Item']['linear_perceptron_weekly']
+            predictions['EXTRA_TREES'] = response['Item']['extra_trees_weekly']
             predictions['HAS_CHANGED'] = response['Item']['week_change']
             company_predictions[sector] = predictions
         return company_predictions
@@ -51,6 +60,9 @@ class PredictionWrapper:
             predictions['TOTAL'] = response['Item']['total_average_monthly']
             predictions['NAIVE_BAYES'] = response['Item']['naive_bayes_monthly']
             predictions['SUPPORT_VECTOR_MACHINE'] = response['Item']['support_vector_machine_monthly']
+            predictions['LINEAR_PERCEPTRON'] = response['Item']['linear_perceptron_monthly']
+            predictions['K_NEIGHBORS_MACHINE'] = response['Item']['k_nearest_monthly']
+            predictions['EXTRA_TREES'] = response['Item']['extra_trees_monthly']
             predictions['HAS_CHANGED'] = response['Item']['month_change']
             company_predictions[sector] = predictions
         return company_predictions
