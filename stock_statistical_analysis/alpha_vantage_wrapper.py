0392b1b620a3d52468170ac3788383b29c87371b
Sam Johnson <sam@3ev.com>
2018-04-16 18:49:48 +0100

    remove deadline from over methods called

--- stock_statistical_analysis/alpha_vantage_wrapper.py
+++ stock_statistical_analysis/alpha_vantage_wrapper.py
@@ -27,7 +27,6 @@ class AlphaVantage:
       return decorate
 
 
-    @deadline(10)
     def get_intraday(self,company="AAPL",timegap="60min"):
         """Calls AlphaVantages API and gets the intraday figures for the given company.
         Args:
@@ -41,7 +40,6 @@ class AlphaVantage:
         data, meta_data = self.ts.get_intraday(symbol=company,interval=timegap)
         return data
 
-    @deadline(10)
     def get_days_information(self,company="AAPL"):
         """Calls AlphaVantages API and gets the days figures for the given company.
         Args:
@@ -53,7 +51,6 @@ class AlphaVantage:
         data, meta_data = self.ts.get_daily(symbol=str(company))
         return data
 
-    @deadline(10)
     def get_weekly_information(self,company="AAPL"):
         """Calls AlphaVantages API and gets the weeks figures for the given company.
         Args:
@@ -64,7 +61,6 @@ class AlphaVantage:
         data, meta_data = self.ts.get_weekly(symbol=str(company))
         return data
 
-    @deadline(10)
     def get_monthly_information(self,company="AAPL"):
         """Calls AlphaVantages API and gets the months figures for the given company.
         Args:
@@ -76,26 +72,23 @@ class AlphaVantage:
         data, meta_data = self.ts.get_monthly(symbol=str(company))
         return data
 
-    @deadline(10)
     def get_sector_intraday(self):
         sp = SectorPerformances(key='QL2Z176B6Q3JYM6A', output_format='pandas')
         data, meta_data = self.sp.get_sector()
         realtime = data['Rank A: Real-Time Performance']
         return realtime.get('Information Technology')
 
-    @deadline(10)
+
     def get_sector_daily(self):
         data, meta_data = self.sp.get_sector()
         day = data['Rank B: Day Performance']
         return day.get('Information Technology')
 
-    @deadline(10)
     def get_sector_weekly(self):
         data, meta_data = self.sp.get_sector()
         week = data['Rank C: Day Performance']
         return week.get('Information Technology')
 
-    @deadline(10)
     def get_sector_monthly(self):
         data, meta_data = self.sp.get_sector()
         month = data['Rank D: Month Performance']
